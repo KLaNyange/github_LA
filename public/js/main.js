c558bc700a31da9cb802fb9890829004318edf66
KLaNyange <nyangealixelambrette@gmail.com>
2023-11-23 10:11:07 +0100

    import et export

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,41 @@
-let cs29 = "vous etes trop cool sauf Quentin"
+import { histoire } from  './module/function.js';
+histoire()
 
-console.log(cs29);
+//objets
+// let alixe = {
+//     nom:"Alixe", 
+//     age:27
+// }
+// let couteau = {
+//     nom:"couteau", 
+//     action:'coupé'
+// }
+// let maison = {
+//     nom:"maison", 
+//     personne:[]
+// }
 
-let loic = {
-    nom : "Loic"
-}
+//class
+// class Cs29{
+//     constructor(prenom, sexe, age){
+//         this.prenom = prenom,
+//         this.sexe = sexe,
+//         this.age = age
+//     }
+//     sePresenter(){
+//         console.log(`coucou je suis ${this.prenom}`);
+//     }
+// }
 
-console.log(loic);
+//instances
+// let ranya = new Cs29("Ranya", "F", 22);
+// let mouad = new Cs29("Mouad", "M", 26);
+
+
+//function
+// function histoire() {
+//     console.log("il etait un fois deux personnage");
+//     ranya.sePresenter()
+//     mouad.sePresenter()
+//     console.log("Fin de l'histoire");
+// }
---
+++ public/js/module/instances.js
@@ -0,0 +1,3 @@
+import { Cs29 } from  './class.js';
+export let ranya = new Cs29("Ranya", "F", 22);
+export let mouad = new Cs29("Mouad", "M", 26);
